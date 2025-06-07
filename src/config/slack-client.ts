d43331941144bc97bbfb7c8e90de46c21dc36125
r-wata <ryusei.watanabe@fuji.waseda.jp>
2025-06-07 14:55:33 +0900

    fix: remove unused list channels handler and update related documentation

--- src/config/slack-client.ts
+++ src/config/slack-client.ts
@@ -2,7 +2,7 @@ import { WebClient } from '@slack/web-api';
 
 export interface SlackClients {
   botClient: WebClient;
-  userClient?: WebClient;
+  userClient: WebClient;
 }
 
 export const SlackContext: SlackClients = {} as SlackClients;
